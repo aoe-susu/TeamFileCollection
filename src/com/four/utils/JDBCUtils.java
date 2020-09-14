3058cf893fd5f8e56c646cb155ba302ac972f7b3
ClassicGraph <1193943971@qq.com>
2020-09-14 11:30:57 +0800

    本版本是初始版本 v1.0

---
+++ src/com/four/utils/JDBCUtils.java
@@ -0,0 +1,23 @@
+package com.four.utils;
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+public class JDBCUtils {
+    private static DataSource dataSource;
+    static {
+        try {
+            Properties properties = new Properties();
+            properties.load(JDBCUtils.class.getClassLoader().getResourceAsStream("druid.properties"));
+            dataSource = DruidDataSourceFactory.createDataSource(properties);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static DataSource getDataSource(){
+        return dataSource;
+    }
+}
